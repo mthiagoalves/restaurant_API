f0e7ab76f974bb6610f229fe9374544142da4fdb
mthiagoalves <m.thiago.alves@live.com>
2023-09-21 16:45:43 +0100

    feat(userControler & userRepository): implement create endpoint to User

--- app/Http/Controllers/Api/v1/UserController.php
+++ app/Http/Controllers/Api/v1/UserController.php
@@ -19,19 +19,13 @@ class UserController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        $dataUser = $request->all();
+
+        return UserRepository::storeUser($dataUser);
     }
 
     /**
--- app/Http/Repositories/v1/UserRepository.php
+++ app/Http/Repositories/v1/UserRepository.php
@@ -4,9 +4,14 @@ namespace App\Http\Repositories\v1;
 
 use App\Http\Resources\v1\UserResource;
 use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use App\Traits\HttpResponses;
+
 
 class UserRepository
 {
+    use HttpResponses;
+
     public static function getAllUsers()
     {
         return UserResource::collection(User::all());
@@ -15,5 +20,35 @@ class UserRepository
     public static function getOneUsers($id)
     {
         return new UserResource(User::where('id', $id)->first());
+    }
+
+    public static function storeUser($dataUser)
+    {
+        $validator = Validator::make($dataUser, [
+            "name" => "string|max:150|required",
+            "username" => "string|max:150|required",
+            "email" => "email|required",
+            "email_verified_at" => "nullable",
+            "password" => ["string", "required", "regex:/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/"],
+            "remember_token" => "string|nullable"
+        ]);
+
+        if ($validator->fails()) {
+            return HttpResponses::error('Data Invalid', 422, $validator->errors());
+        }
+
+        if (User::where('username', $validator->validated()["username"])->exists()) {
+            return HttpResponses::error('Username already exist, please insert another.', 422);
+        } else if (User::where('email', $validator->validated()["email"])->exists()) {
+            return HttpResponses::error('Email already registred, please insert another.', 422);
+        }
+
+        $created = User::create($validator->validated());
+
+        if ($created) {
+            return HttpResponses::success('User created successfully', 200, new UserResource($created));
+        }
+
+        return HttpResponses::error('Something wrong to create user', 400);
     }
 }
