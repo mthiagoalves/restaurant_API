5d39905fd8a3a228963484bee015ad1a25fc851e
mthiagoalves <m.thiago.alves@live.com>
2023-09-21 16:44:31 +0100

    feat(HttpResponses): change public function to public static function

--- app/Traits/HttpResponses.php
+++ app/Traits/HttpResponses.php
@@ -9,7 +9,7 @@ use Illuminate\Support\MessageBag;
 
 trait HttpResponses
 {
-    public function success(string $message, string|int $status, array|Model|JsonResource $data = [])
+    public static function success(string $message, string|int $status, array|Model|JsonResource $data = [])
     {
         return response()->json([
             'message' => $message,
@@ -18,7 +18,7 @@ trait HttpResponses
         ], $status);
     }
 
-    public function error(string $message, string|int $status, array|MessageBag $errors = [], array $data = [])
+    public static function error(string $message, string|int $status, array|MessageBag $errors = [], array $data = [])
     {
         return response()->json([
             'message' => $message,
