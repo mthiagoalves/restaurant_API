163904030514d93f3259129645c13acb81f6af64
mthiagoalves <m.thiago.alves@live.com>
2023-09-26 17:56:27 +0100

    feat(userResouce): implement validation cause user that deleted

--- app/Http/Resources/v1/UserResource.php
+++ app/Http/Resources/v1/UserResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\v1;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +15,14 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        if($this->deleted_at != null) {
+            return [
+                'name' => $this->name,
+                'username' => $this->username,
+                'email' => $this->email,
+                'deleted' => Carbon::parse($this->deleted_at)->format('d/m/Y - H:i:s')
+            ];
+        }
         return [
             'name' => $this->name,
             'username' => $this->username,
