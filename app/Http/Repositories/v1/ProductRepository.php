e76f566658d8f26c3aeb16b40ae5489ec82fc6b7
mthiagoalves <m.thiago.alves@live.com>
2023-09-28 10:23:58 +0100

    feat(productRepository): implement getOne endpoint

--- app/Http/Repositories/v1/ProductRepository.php
+++ app/Http/Repositories/v1/ProductRepository.php
@@ -4,6 +4,8 @@ namespace App\Http\Repositories\v1;
 
 use App\Http\Resources\v1\ProductResource;
 use App\Models\Product;
+use App\Traits\HttpResponses;
+
 
 class ProductRepository
 {
@@ -14,5 +16,23 @@ class ProductRepository
 
     public static function getOneProduct($productId)
     {
+        self::verifyProductExistent($productId);
+
+        $product = Product::where('id', $productId)->first();
+
+        if($product == null) {
+            $productTrashed = Product::onlyTrashed()->find($productId);
+            return HttpResponses::success('Product was deleted', 200, new ProductResource($productTrashed));
+        }
+
+        return new ProductResource($product);
+    }
+
+    private static function verifyProductExistent($productId) {
+        $product = Product::find($productId);
+
+        if(!$product){
+            return HttpResponses::error('Product not found', 404);
+        }
     }
 }
