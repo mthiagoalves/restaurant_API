2a191d1a6c5b81f5753108ff833455b5246b213f
mthiagoalves <m.thiago.alves@live.com>
2023-09-28 17:31:14 +0100

    feat(Category): create resource controller and config categoryResource

--- app/Http/Resources/v1/CategoryResource.php
+++ app/Http/Resources/v1/CategoryResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +15,18 @@ class CategoryResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        if ($this->deleted_at != null) {
+            return [
+                'name' => $this->name,
+                'slug' => $this->slug,
+                'deleted' => Carbon::parse($this->deleted_at)->format('d/m/Y - H:i:s')
+            ];
+        }
+
+        return [
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'order' => $this->order,
+        ];
     }
 }
